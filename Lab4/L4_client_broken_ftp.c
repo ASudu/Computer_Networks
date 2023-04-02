40829d2774d036472a7d1af8c46c47bc602143e7
A.Sudarshan <66859664+ASudu@users.noreply.github.com>
2023-04-02 22:41:16 +0530

    Update L4_client_broken_ftp.c

--- Lab4/L4_client_broken_ftp.c
+++ Lab4/L4_client_broken_ftp.c
@@ -20,8 +20,16 @@ int main()
 {
     int c_sock, command, offset, recvlen=0;
     char rcvbuff[10];
+    // Buffer to send file offset value
     unsigned char buff_offset[10];
+    // Buffer to send complete file (0) or partial file (1)
     unsigned char buff_command[2];
+
+    memset(rcvbuff, '\0', sizeof(rcvbuff));
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(server));
+
+    /* CREATE UDP socket*/
     
     return 0;
 }
