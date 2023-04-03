6b84c3e1704a771c5c524b32eaea6ca0319548a1
A.Sudarshan <66859664+ASudu@users.noreply.github.com>
2023-04-03 19:51:39 +0530

    broken ftp client

--- Lab4/L4_client_broken_ftp.c
+++ Lab4/L4_client_broken_ftp.c
@@ -19,7 +19,8 @@ void error(char* msg)
 int main()
 {
     int c_sock, command, offset, recvlen=0;
-    char rcvbuff[10];
+    char* server_ip = "172.18.76.160";
+    char rcvbuff[256];
     // Buffer to send file offset value
     unsigned char buff_offset[10];
     // Buffer to send complete file (0) or partial file (1)
@@ -29,7 +30,65 @@ int main()
     struct sockaddr_in server;
     memset(&server, 0, sizeof(server));
 
-    /* CREATE UDP socket*/
+    /* CREATE TCP socket*/
+    if((c_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        error("Error in creating client socket!....Exiting\n");
+    else
+        printf("Client socket created!\n");
     
+    /* Populate address structure*/
+    server.sin_family = AF_INET;
+    server.sin_port = htons(5001);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    /* CONNECT attempt*/
+    if(connect(c_sock, (struct sockaddr*) &server, sizeof(server)) < 0)
+        error("Error in connecting to server!....Exiting\n");
+    
+    /* Create file where data will be stored*/
+    FILE* fp;
+    fp = fopen("dest_file.txt", "ab");
+    if(!fp)
+        error("Error opening the file!....Exiting\n");
+    // Go to end of file
+    fseek(fp, 0, SEEK_END);
+    // Get file size
+    offset = ftell(fp);
+    fclose(fp);
+
+    fp = fopen("dest_file.txt", "ab");
+    if(!fp)
+        error("Error opening the file!....Exiting\n");
+    
+    printf("Enter (0) to get complete file, (1) to specify offset, (2) calculate offset value from local file: \n");
+    scanf("%d", &command);
+    sprintf(buff_command, "%d", command);
+    write(c_sock, buff_command, 2);
+
+    if(command == 1 || command == 2)
+    {
+        if(command == 1)
+        {
+            printf("Enter the value of file offset: \n");
+            scanf("%d", &offset);
+
+        }
+
+        // Else we have offset value already
+        sprintf(buff_offset, "%d", offset);
+        write(c_sock, buff_offset, 10);
+    }
+
+    /* RECEIVE data in chunks of 256 bytes*/
+    while((recvlen = read(c_sock, rcvbuff, 256)) > 0)
+    {
+        printf("Bytes received: %d\n", recvlen);
+        fwrite(rcvbuff, 1, recvlen, fp);
+        memset(rcvbuff, '\0', sizeof(rcvbuff));
+    }
+    
+    if(recvlen < 0)
+        error("Error in reading file!....Exiting\n");
+
     return 0;
 }
