50126dc41f0b5eb4253d1cfa12579bca2cd5406f
A.Sudarshan <66859664+ASudu@users.noreply.github.com>
2023-04-20 17:32:07 +0530

    Lab6 started

---
+++ Lab6/rdt_npl_c.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFLEN 512
+#define PORT 8882
+
+typedef struct packet1
+{
+    int sq_no;
+}ACK_PKT;
+
+typedef struct packet2
+{
+    int sq_no;
+    char data[BUFLEN];
+}DATA_PKT;
+
+void error(char* msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main()
+{
+    struct sockaddr_in client;
+    int c_sock, i, clen=sizeof(client);
+    char buf[BUFLEN], message[BUFLEN];
+    char* server_ip = "127.0.1.1";
+    DATA_PKT send_pkt,rcv_ack;
+    
+    if ( (c_sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        error("[-] Error creating client socket! Exiting...\n");
+    
+    memset((char *) &client, 0, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORT);
+    client.sin_addr.s_addr = inet_addr(server_ip);
+    
+    int state = 0;
+    while(1)
+    {
+        switch(state)
+        {
+            case 0:     // wait for sending packet with seq. no. 0
+                printf("Enter message 0: ");
+                fgets(send_pkt.data,sizeof(send_pkt),stdin);
+                send_pkt.sq_no = 0;
+                if  (sendto(c_sock,  &send_pkt,  sizeof(send_pkt),  0,  (struct sockaddr *) &client, clen)==-1)
+                   error("[-] Error in sending message with seq #0! Exiting...\n");
+                state = 1;
+                break;
+            
+            case 1:     // Waiting for ACK 0
+                if (recvfrom(c_sock, &rcv_ack, sizeof(rcv_ack), 0, (struct sockaddr *) &client, &clen) == -1)
+                    error("[-] Error in receiving ACK 0! Exiting...\n");
+                if (rcv_ack.sq_no==0)
+                {  
+                    printf("[+] Received ack seq. no. %d\n",rcv_ack.sq_no);
+                    state = 2;
+                    break;
+                }
+            case 2:     //wait for sending packet with seq. no. 1
+                printf("Enter message 1: ");
+                fgets(send_pkt.data,sizeof(send_pkt),stdin);
+                send_pkt.sq_no = 1;
+                if  (sendto(c_sock,  &send_pkt,  sizeof(send_pkt)  ,  0  ,  (struct sockaddr *) &client, clen)==-1)
+                    error("[-] Error in sending message with seq #1! Exiting...\n");
+                state = 3;
+                break;
+            case 3:     //waiting for ACK 1
+                if(recvfrom(c_sock, &rcv_ack, sizeof(rcv_ack), 0, (struct sockaddr*) &client, &clen) == -1)
+                    error("[-] Error in receiving ACK 1! Exiting...\n");
+                if (rcv_ack.sq_no==1)
+                {
+                    printf("[+] Received ack seq. no. %d\n",rcv_ack.sq_no);
+                    state = 0;
+                    break;
+                }
+        }
+    }
+
+    close(c_sock);    
+    return 0;
+}
---
+++ Lab6/rdt_npl_s.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFLEN 512
+#define PORT 8882
+
+typedef struct packet1
+{
+    int sq_no;
+}ACK_PKT;
+
+typedef struct packet2
+{
+    int sq_no;
+    char data[BUFLEN];
+}DATA_PKT;
+
+void error(char* msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main()
+{
+    struct sockaddr_in server, client;
+    int s_sock, i, recv_len, slen = sizeof(client);
+    DATA_PKT rcv_pkt;
+    ACK_PKT  ack_pkt;
+
+    // Create a UDP socket
+    if ((s_sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        error("[-] Error creating server socket! Exiting...\n");
+
+    // Zero out the structure
+    memset((char *) &server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    // Bind socket to port
+    if( bind(s_sock , (struct sockaddr*)&server, sizeof(server) ) == -1)
+        error("[-] Error in binding server socket! Exiting...\n");
+    
+    int state =0;
+
+    while(1)
+    {
+        switch(state)
+        {
+            case 0:
+            {
+                printf("Waiting for packet 0 from sender...\n");
+                fflush(stdout);
+                // Try to receive some data, this is a blocking call
+                if ((recv_len = recvfrom(s_sock, &rcv_pkt, BUFLEN, 0, (struct sockaddr*) &client, &slen)) == -1)
+                    error("[-] Error in receiving message with seq #0! Exiting...\n");
+                if (rcv_pkt.sq_no==0)
+                {
+                    printf("[+] Packet received with seq. no. %d and Packet content is:\n %s",rcv_pkt.sq_no, rcv_pkt.data);
+                    ack_pkt.sq_no = 0;
+                    if  (sendto(s_sock,  &ack_pkt,  recv_len,  0,  (struct  sockaddr*)  &client, slen) == -1)
+                        error("[-] Error in sending ACK 0! Exiting...\n");
+                    state = 1;
+                    break;
+                }
+            }
+
+            case 1:
+            {
+                printf("Waiting for packet 1 from sender...\n");
+                fflush(stdout);
+                // Try to receive some data, this is a blocking call
+                if ((recv_len = recvfrom(s_sock, &rcv_pkt, BUFLEN, 0, (struct sockaddr*) &client, &slen)) == -1)
+                    error("[-] Error in receiving message with seq #1! Exiting...\n");
+                if (rcv_pkt.sq_no==1)
+                {
+                    printf("[+] Packet received with seq. no. %d and Packet content is: %s",rcv_pkt.sq_no, rcv_pkt.data);
+                    ack_pkt.sq_no = 1;
+                    if  (sendto(s_sock,  &ack_pkt,  recv_len,  0,  (struct  sockaddr*)  &client, slen) == -1)
+                        error("[-] Error in sending ACK 1! Exiting...\n");
+                    state = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    close(s_sock);
+    return 0;
+}
